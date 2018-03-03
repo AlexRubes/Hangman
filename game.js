fceafc6ef4474f44d87e0ac8ca520ab0e9380148
Alejandra Rubalcava <arubalcava@group.on>
2018-03-02 18:48:59 -0600

    letters going in right place and accounting for multiples

--- game.js
+++ game.js
@@ -3,49 +3,50 @@
 //create array of words to guess
 let words = ["bulls", "hornets", "warriors", "wizards", "suns"];
 let guesses = 10;
-let userGuess = document.getElementById("already-guess");
 let lettersGuessed = [];
+let guessIndex = -1;
+let numberSpaces = [];
+
+
+//on page load the game chooses a word
+let randomNumber = Math.floor(Math.random()*5);
+let compChoice = words[randomNumber];
+
+//break letters of chosen word into array
+let compArray = Array.from(compChoice);
+console.log(compArray);
+
+for (var i=0; i < compChoice.length; i++) {
+    numberSpaces.push("_");
+}
 
 //game starts on first click
 document.onkeyup = function (event) {
     //take letter pressed as an event aka userText
-    userText = event.key;
-    //push userText into an array
-    lettersGuessed.push(userText);
-    document.getElementById("already-guess").innerHTML = lettersGuessed;
-    console.log(userText);
-    console.log(lettersGuessed);
-
-    console.log(compArray.includes(userText));
-
-    if (compArray.includes(userText)) {
-        console.log("it works!");
-        //if userText is a letter in compArray, put in current word
-        document.getElementById("blank-blank").innerHTML = userText;
+    userText = event.key; 
+    
+     if (compChoice.includes(userText) === true) {
+        for (var i=0; i < compChoice.length; i++) {
+            if (compChoice[i] === userText) {
+                numberSpaces[i] = userText;
+                document.getElementById("blank-blank").innerHTML = numberSpaces.join(" ");
+            }
+        } 
     }
-}
-//on page load the game chooses a word
-    let randomNumber = Math.floor(Math.random()*5);
-    let compChoice = words[randomNumber];
+    
+    else { 
+            lettersGuessed.push(userText);
+            document.getElementById("already-guess").innerHTML = lettersGuessed;
+            console.log(userText);
+            console.log(lettersGuessed);
+            console.log(compArray.includes(userText));
+            }
+        }
 
-//break letters of chosen word into array
-    let compArray = Array.from(compChoice);
-    console.log(compArray);
+
+
 
 //screen displays dash marks that correspond to word length
-window.onload = function() {
-    let numberSpaces = compArray.fill("_");
-    console.log(compArray.fill("_"));
-    document.getElementById("blank-blank").innerHTML = numberSpaces;
-};
-
-//if true, assign letter to correct position on screen
-
-//use indexof to get position of the letter
-if a in word let location = word.indexof ('a')
-//sets location to zero
-
-//use splice to replace dashes with letters
 
 
 //run loop until you cover all the spaces that have that correct letter
@@ -53,6 +54,4 @@ if a in word let location = word.indexof ('a')
 //with each letter guessed, decrese the number of guesses by 1
 //if word is correctly guessed, increase win count by 1 and restart process
 //if user runs out of guesses (=0), display you lose
-
-//.join("") turns array into string
 
